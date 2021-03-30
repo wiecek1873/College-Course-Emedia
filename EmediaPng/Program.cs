22578ec9263a198de8a61c5306693244f408aa92
Górka Mateusz <maatiug@gmail.com>
2021-03-30 15:56:32 +0200

    new console project

---
+++ EmediaPng/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmediaPng
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
