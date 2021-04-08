c7a42bc896382aafdb2c194c6d809761a716930a
Górka Mateusz <maatiug@gmail.com>
2021-04-08 10:01:02 +0200

    IHDR chunk

--- EmediaPng/Chunks/IHDR.cs
+++ EmediaPng/Chunks/IHDR.cs
@@ -2,8 +2,26 @@ namespace EmediaPng
 {
     internal class IHDR : Chunk
     {
+        public uint Width {get; private set;}
+        public uint Height {get; private set;}
+        public byte ColorType {get; private set;}
+        public byte CompressionMethod {get; private set;}
+        public byte FilterMethod {get; private set;}
+        public byte InterlanceMethod {get; private set;}
+
         public IHDR(char[] type, uint length, byte[] data, byte[] crc) : base(type, length, data, crc)
         {
+            Width = DataReader.ReadUint32(data, 0);
+            Height = DataReader.ReadUint32(data, 4);
+            ColorType = data[8];
+            CompressionMethod = data[9];
+            FilterMethod = data[10];
+            InterlanceMethod = data[11];
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} | size: {Width} x {Height} | color: {ColorType} | compression: {CompressionMethod} | filter: {FilterMethod} | interlance: {InterlanceMethod}";
         }
     }
 }
--- EmediaPng/DataReader.cs
+++ EmediaPng/DataReader.cs
@@ -2,13 +2,13 @@ namespace EmediaPng
 {
     public static class DataReader
     {
-        public static uint ReadUint32(byte[] bytes)
+        public static uint ReadUint32(byte[] bytes, int startIndex = 0)
         {
             uint value = 0;
-            foreach (byte b in bytes)
+            for (int i = startIndex; i < startIndex + 4; ++i)
             {
                 value <<= 8;
-                value += (uint) b;
+                value += (uint) bytes[i];
             }
             return value;
         }
