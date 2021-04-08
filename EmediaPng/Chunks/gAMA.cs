9ad416febf16c78d70df16001f7959b0f0295df7
DESKTOP-1VCEAC1\Michal <wiecek1873@gmail.com>
2021-04-08 17:13:58 +0200

    Dodatkowe obsługiwane chunki

---
+++ EmediaPng/Chunks/gAMA.cs
@@ -0,0 +1,17 @@
+﻿namespace EmediaPng
+{
+	internal class gAMA : Chunk
+	{
+		public uint Gamma { get; private set; }
+
+		public gAMA(char[] type, uint length, byte[] data, byte[] crc) : base(type, length, data, crc)
+		{
+			Gamma = DataReader.ReadUint32(data, 0);
+		}
+
+		public override string ToString()
+		{
+			return $"{base.ToString()} | gamma: {Gamma}";
+		}
+	}
+}
