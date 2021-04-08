e14dcc051e76c3458ebb53fce2e1f08947d27af6
DESKTOP-1VCEAC1\Michal <wiecek1873@gmail.com>
2021-04-08 16:33:12 +0200

    Możliwość wyboru wielu przykładowych obrazów

--- EmediaPng/Program.cs
+++ EmediaPng/Program.cs
@@ -1,14 +1,32 @@
 ﻿using System;
+using System.IO;
 
 namespace EmediaPng
 {
-    class Program
-    {
-        private const string testFile = "../../../FileExamples/c-sharp-logo.png";
+	class Program
+	{
 
-        static void Main(string[] args)
-        {
-            var png = new PngParser(testFile);
-        }
-    }
+		private const string examples = "../../../FileExamples/";
+
+		static void Main(string[] args)
+		{
+			string[] files = Directory.GetFiles(examples);
+
+			Console.WriteLine("Choose image");
+
+			for(int i =0; i < files.Length; i++)
+			{
+				Console.Write(i + ": ");
+				Console.WriteLine(files[i].Substring(examples.Length));
+			}
+
+			int choice = Convert.ToInt32(Console.ReadLine());
+			if (0 <= choice && choice < files.Length)
+			{
+				var png = new PngParser(files[choice]);
+			}
+			else
+				throw new Exception();
+		}
+	}
 }
