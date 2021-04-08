5a104ad967d0b62af0e13f37b37e9731d2b55939
DESKTOP-1VCEAC1\Michal <wiecek1873@gmail.com>
2021-04-08 16:32:48 +0200

    Dodana obsługa PLTE

--- EmediaPng/Chunks/PLTE.cs
+++ EmediaPng/Chunks/PLTE.cs
@@ -1,9 +1,38 @@
+using System;
 namespace EmediaPng
 {
-    internal class PLTE : Chunk
-    {
-        public PLTE(char[] type, uint length, byte[] data, byte[] crc) : base(type, length, data, crc)
-        {
-        }
-    }
+	internal class PLTE : Chunk
+	{
+		public class Palette
+		{
+			public byte Red;
+			public byte Green;
+			public byte Blue;
+
+			public Palette(byte red, byte green, byte blue)
+			{
+				Red = red;
+				Green = green;
+				Blue = blue;
+			}
+		}
+
+		public Palette[] Palettes { get; private set; }
+
+		public PLTE(char[] type, uint length, byte[] data, byte[] crc) : base(type, length, data, crc)
+		{
+			Palettes = new Palette[length / 3];
+
+			int index = 0;
+			for (int i = 0; i < Palettes.Length; i += 3)
+			{
+				Palettes[index] = new Palette(data[i], data[i + 1], data[i + 2]);
+				index++;
+			}
+		}
+		public override string ToString()
+		{
+			return $"{base.ToString()} | pallets count: {Palettes.Length} | Red : {Palettes[0].Red} | Green : {Palettes[0].Green} | Blue : {Palettes[0].Blue}";
+		}
+	}
 }
